50ee3719640e026b891d203d363ceb47ab2e30de
pavan <pavankumar.shetty@dimaag.ai>
2024-12-08 23:21:45 +0530

    doc

--- ChessBoardAnalysis/config.py
+++ ChessBoardAnalysis/config.py
@@ -1,6 +1,6 @@
 class PATH:
-    INPUT_DIR = r"C:\Users\pavan\Desktop\chessBoardAnalysis\chess_testing_images\good_chess2.jpg"
-    OUTPUT_DIR = r"C:\Users\pavan\Desktop\chessBoardAnalysis\output\result.png"
+    INPUT_DIR = r"C:\Users\pavan\Desktop\chess_Assignment\ChessBoardAnalysis\chess_testing_images\good_chess8.png"
+    OUTPUT_DIR = r"C:\Users\pavan\Desktop\chess_Assignment\ChessBoardAnalysis\output\result.png"
     
 class PARAMETERS:
     HOMO_SHAPE = (400,400)
